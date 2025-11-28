d9f16251dff4fd7b78e760a9b37485a6ce1ee06e
Your Name <you@example.com>
2025-11-27 21:17:37 -0500

    BITCH CAN U WOrk

--- a5_code/game/game.cc
+++ a5_code/game/game.cc
@@ -25,17 +25,16 @@ import <iostream>;
 using namespace std; 
 
 export class Game {
-    CommandCenter cmdCenter;
-
     unique_ptr<Player> p1;
     unique_ptr<Player> p2;
     Player* currP;
-
     unique_ptr<TextDisplay> textDisplay;
     // unique_ptr<GraphicDisplay> graphicsDisplay;
-
     int highscore;
     bool textOnly;
+    CommandCenter cmdCenter;
+
+
     // vector<string> args;
 
     public:
